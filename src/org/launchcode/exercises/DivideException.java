5ed550b2dcfb99181744abdee8444d6f0a2e0089
Zoe Smith <107585629+ZoeGrey86@users.noreply.github.com>
2022-09-22 13:09:31 -0500

    done.

---
+++ src/org/launchcode/exercises/DivideException.java
@@ -0,0 +1,8 @@
+package org.launchcode.exercises;
+
+public class DivideException extends Exception{
+    public DivideException(String message){
+        super(message);
+    }
+
+}
--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -15,17 +15,45 @@ public class Main {
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
 
+        //test divide method
+        //System.out.println(Divide(12,2));
+
         // Test out your CheckFileExtension() function!
+        for(Map.Entry<String, String> student: studentFiles.entrySet()){
+            String studentFile = student.getValue();
+            System.out.println(student.getKey() + ", " +CheckFileExtension(studentFile));
+        }
     }
 
-    public static void Divide(int x, int y)
+    public static int Divide(int x, int y)
     {
         // Write code here!
+        if( y == 0){
+            try{throw new DivideException("Can't divide by zero.");
+            }catch (DivideException e){
+                e.printStackTrace();
+            }
+            System.out.println();
+        }
+        int z = x/y;
+        return z;
     }
 
     public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
+    { int grade = 0;
+        if(fileName.isEmpty() || fileName == null){
+            grade -= 1;
+            try{
+                throw new Exception("Invalid File! Minus one point ");
+            }catch (DivideException e){e.printStackTrace();} catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println();
+        }
+        if(fileName.contains(".java")){
+            grade =+ 1;
+        }else if(!fileName.contains(".java")){grade += 0;}
+        return grade;
     }
 
 }
